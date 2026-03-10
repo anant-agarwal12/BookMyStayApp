0b6fb61733059fd649de064479dbc6ecdd5d90fb
anant <anant22saawara@gmail.com>
2026-03-10 09:19:36 +0530

    Added entry point

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,4 +1,12 @@
+/*
+    Use Case 1 : System entry point and welcome message
+ */
+
 public class HotelBookingApp {
    public static void main(String[] args){
+       System.out.println("Welcome to the Hotel Booking Management System");
+       System.out.println("Version 1.0");
+       System.out.println("System initialized successfully");
    }
 }
+
